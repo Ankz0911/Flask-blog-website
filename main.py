caa6ace049435c54854d6b54a4458448517ec99f
Ankz0911 <ankur.khurana.cool@gmail.com>
2022-04-09 12:29:47 +0530

    some minor upgrades , made the contact form work and send an email to me.

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import requests
+import smtplib
+import os
 
 app = Flask(__name__)
 n_point_api = "https://api.npoint.io/ab312a499fe456df41c6"
+my_email = os.environ.get('MY_EMAIL')
+my_password = os.environ.get('MY_PASSWORD')
+
+
+def send_email(username, usermail, userphone, usermessage):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="ankur.khurana.cool@gmail.com",
+                            msg=f'Subject: Contact Information received \n\n '
+                                f'Name : {username}\n Email : {usermail}\n Phone:{userphone}\n Message: {usermessage}')
+
+
+@app.route('/')
+def hello():
+    return redirect('index.html')
 
 
 @app.route('/index.html')
 def home():
     responses = requests.get(n_point_api).json()
-    return render_template("index.html", input_data = responses)
+    return render_template("index.html", input_data=responses)
 
 
 @app.route('/about.html')
@@ -18,7 +37,7 @@ def about_page():
 
 @app.route('/contact.html')
 def contact_me():
-    return render_template("contact.html")
+    return render_template("contact.html", message_sent=True)
 
 
 @app.route("/blog/<int:blog_id>")
@@ -32,6 +51,16 @@ def blog_post(blog_id):
         else:
             pass
     return render_template("post.html", post1_t=post_title, post1_st=post_subtitle, post1_b=post_body)
+
+
+@app.route('/form-entry', methods=["POST"])
+def receive_data():
+    user_name = request.form["name"]
+    user_email = request.form["email"]
+    user_phone = request.form["phone"]
+    user_message = request.form["message"]
+    send_email(user_name, user_email, user_phone, user_message)
+    return render_template("contact.html", message_sent=False)
 
 
 if __name__ == "__main__":
--- trial.py
+++ trial.py
@@ -1,4 +1,1 @@
-import requests
-
-response = requests.get("https://api.npoint.io/ab312a499fe456df41c6").json()
-print(response)
+print('Done')
